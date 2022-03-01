33a149672dee20061d2411cc7436b26d059af050
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-03-01 18:29:18 +0000

    Add mkproject method

--- src/pynball/vars.py
+++ src/pynball/vars.py
@@ -1,7 +1,10 @@
 # Core Library modules
+import ast
 import os
 import subprocess
+import sys
 import winreg
+from pathlib import Path
 
 
 class VarControl:
@@ -14,6 +17,19 @@ class VarControl:
         self.system_subkey = (
             r"System\CurrentControlSet\Control\Session Manager\Environment"
         )
+        try:
+            self.workon_home = Path(os.environ["WORKON_HOME"])
+        except KeyError:
+            self.workon_home = Path("")
+        try:
+            self.project_home = Path(os.environ["PROJECT_HOME"])
+        except KeyError:
+            self.project_home = Path("")
+        try:
+            self.pynball = os.environ["PYNBALL"]
+            self.versions = ast.literal_eval(self.pynball)  # type: ignore
+        except KeyError:
+            self.versions = {}  # type: ignore
 
     @staticmethod
     def _execute(*args, supress_exception=False):
@@ -58,7 +74,10 @@ class VarControl:
             key = winreg.CreateKey(self.user_key, self.user_subkey)
         elif scope == "system":
             key = winreg.CreateKey(self.system_key, self.system_subkey)
-        value, _ = winreg.QueryValueEx(key, name)  # noqa
+        try:
+            value, _ = winreg.QueryValueEx(key, name)  # noqa
+        except FileNotFoundError:
+            value = None
         return value
 
     def deletenv(self, scope, name):
@@ -77,11 +96,93 @@ class VarControl:
         except OSError as e:
             print(f"Deletion of key: '{name}' failed -\n {e}")
 
+    def add_version(self, name, version_path):
+        """Adds a friendly name and path of an installation."""
+
+        pass
+
+    def delete_version(self):
+        """Deletes a friendly name and path of an installation."""
+        pass
+
+    def clear_versions(self):
+        """Delete all friendly names and paths"""
+
+    def version(self):
+        """Returns details about the current Python version."""
+        print("{0.major}.{0.minor}.{0.micro}".format(sys.version_info))
+        pass
+
+    def versions(self):
+        """Lists the names of the python installs."""
+        pass
+
+    def switchto(self):
+        """Changes the version of python."""
+        pass
+
+    def find(self):
+        """Finds installations of python."""
+        pass
+
+    def pythonpath(self):
+        """Sets environment variable."""
+        pass
+
+    def mkproject(self, ver, project_name):
+        """Makes a virtual environment from a specific version."""
+        if self.workon_home is None or self.project_home is None:
+            print(
+                """Virtualenv-wrapper is not configured on you system:
+            Please install Virtualenv and Virtualenv-wrapper and configure
+            'WORKON_HOME' and 'PROJECT_HOME' environment variables"""
+            )
+            return
+        for directory in [self.workon_home, self.project_home]:
+            new_path = directory / project_name
+            try:
+                new_path.mkdir(parents=False, exist_ok=False)
+                if directory == self.workon_home:
+                    self._execute(
+                        "virtualenv",
+                        r"-p=D:\PYTHON\python3.8\python.exe",
+                        str(new_path),
+                    )
+                    (new_path / ".project").touch()
+                    (new_path / ".project").write_text(
+                        f"{self.project_home / project_name}"
+                    )
+            except FileNotFoundError:
+                print(
+                    f"Project: '{project_name}' has NOT be created - {directory} "
+                    f"does not exist"
+                )
+                return
+            except FileExistsError:
+                print(f"""The directory '{new_path}' already exits""")
+                return
+
+    def rmproject(self, ver, project_name):
+        """Deletes a virtual environment from a specific version."""
+        pass
+
+    def export_config_file(self):
+        """Creates a configuration file backup"""
+        pass
+
+    def import_config_file(self):
+        """Creates a configuration file backup"""
+        pass
+
 
 x = VarControl()
+"""
 x.setenv("system", "STEVE", "123")
 x.setenv("user", "STEVE", "456")
 print(x.getenv("system", "STEVE"))
 print(x.getenv("user", "STEVE"))
 x.deletenv("system", "STEVE")
 x.deletenv("user", "STEVE")
+"""
+
+x.mkproject(3, "steve")
