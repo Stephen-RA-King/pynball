f88d45aeaa9a60fd2355100b410addd642f272c9
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-03-06 22:52:31 +0000

    Change order of meta data

--- src/pynball/__init__.py
+++ src/pynball/__init__.py
@@ -1,9 +1,9 @@
 """Top-level package for pynball."""
 
 __title__ = "pynball"
+__version__ = "0.1.0"
 __author__ = "Stephen R A King"
 __description__ = "Utility command line to switch between python versions"
 __email__ = "stephen.ra.king@gmail.com"
-__version__ = "0.1.0"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022 Stephen R A King"
