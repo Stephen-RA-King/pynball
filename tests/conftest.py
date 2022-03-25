44b5f0c8b1b2d5f3e4b6176d017ab47ab994d78a
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-03-25 19:59:25 +0000

    chore: Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,106 @@
+# Core Library modules
+import shutil
+from pathlib import Path
+
+# Third party modules
+import pytest
+
+# First party modules
+from pynball import pynball
+
+BASE_DIR = Path(__file__).parents[1]
+TEST_ENV = BASE_DIR / "env"
+WORKON_HOME = TEST_ENV / "virtual_env" / "pyvenv"
+PROJECT_HOME = TEST_ENV / "virtual_env" / "dev"
+PYENV_HOME = TEST_ENV / "versions"
+PYTHON_DIR = TEST_ENV / "PYTHON"
+
+
+def pytest_configure():
+    pytest.BASE_DIR = Path(__file__).parents[1]
+    pytest.TEST_ENV = BASE_DIR / "env"
+    pytest.WORKON_HOME = TEST_ENV / "virtual_env" / "pyvenv"
+    pytest.PROJECT_HOME = TEST_ENV / "virtual_env" / "dev"
+    pytest.PYENV_HOME = TEST_ENV / "versions"
+    pytest.PYTHON_DIR = TEST_ENV / "PYTHON"
+
+
+path1 = str(PYTHON_DIR / "3.9.10")
+path2 = str(PYTHON_DIR / "3.9.10" / "Scripts")
+path_env = "".join(
+    ["Path=", path1, "\\", ";", path2, "\\", ";", r"C:\WINDOWS\system32;C:\WINDOWS"]
+)
+
+
+newdict = {}
+for vers in ["3.6.8", "3.7.9", "3.9.10"]:
+    penv = PYTHON_DIR / vers
+    newdict[vers] = penv
+pynball_raw_dict = {name: str(path) for name, path in newdict.items()}
+pynball_env = str(pynball_raw_dict)
+
+
+@pytest.fixture()
+def create_env():
+    pyenv_ver = ["3.5.4", "3.9.10", "3.10.3"]
+    python_ver = [
+        ("3.6.8", "n"),
+        ("3.7.9", "y"),
+        ("3.8.10", "y"),
+        ("3.9.10", "y"),
+        ("3.10.2", "y"),
+    ]
+    TEST_ENV.mkdir()
+    ve_dir = TEST_ENV / "virtual_env"
+    ve_dir.mkdir()
+    WORKON_HOME.mkdir()
+    PROJECT_HOME.mkdir()
+
+    PYENV_HOME.mkdir()
+    for ver in pyenv_ver:
+        (PYENV_HOME / ver).mkdir()
+
+    PYTHON_DIR.mkdir()
+    for ver in python_ver:
+        name, mkfile = ver
+        pyver = PYTHON_DIR / name
+        pyver.mkdir()
+        if mkfile == "y":
+            (pyver / "python.exe").touch()
+            (pyver / "python.exe").write_text(f"This is version {name}")
+    yield
+    shutil.rmtree(TEST_ENV)
+
+
+@pytest.fixture
+def mock_check_pyenv(monkeypatch):
+    def mock_checkpyenv(*args, **kwargs):
+        return 1
+
+    monkeypatch.setattr(pynball, "_check_pyenv", mock_checkpyenv)
+
+
+@pytest.fixture
+def mock_get_env(monkeypatch):
+    def mock_getenv(scope, name):
+        if scope == "user" and name == "PYNBALL":
+            return pynball_env
+        elif scope == "system" and name == "PATH":
+            return path_env
+        else:
+            return
+
+    monkeypatch.setattr(pynball, "_getenv", mock_getenv)
+
+
+@pytest.fixture
+def mock_set_env(monkeypatch):
+    def mock_setenv(scope, name, value):
+        if scope == "user" and name == "PYNBALL":
+            return value
+        elif scope == "system" and name == "PATH":
+            return value
+        else:
+            return
+
+    monkeypatch.setattr(pynball, "_setenv", mock_setenv)
