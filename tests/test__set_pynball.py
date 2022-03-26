5b7caa1f99840c23126b9a5f462257add2b1702a
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-03-26 20:26:11 +0000

    chore: Initial commit

---
+++ tests/test__set_pynball.py
@@ -0,0 +1,16 @@
+# Core Library modules
+from pathlib import Path
+
+# Third party modules
+import pytest
+
+# First party modules
+from pynball import pynball
+
+
+def test_set_pynball1(capsys, mock_set_env):
+    dict_object = {"3.6": Path("D:\\Python\\python3.6")}
+    pynball._set_pynball(dict_object)
+    output, error = capsys.readouterr()
+    assert output == "{'3.6': 'D:\\\\Python\\\\python3.6'}\n"
+    assert error == ""
