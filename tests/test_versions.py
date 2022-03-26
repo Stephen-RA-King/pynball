1deaba2899904f7bbb4eb5d4dbee1d8ae4d0181c
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-03-26 08:57:09 +0000

    chore: Initial commit

---
+++ tests/test_versions.py
@@ -0,0 +1,25 @@
+# Core Library modules
+from pathlib import Path, PurePath, WindowsPath
+
+# Third party modules
+import pytest
+from click.testing import CliRunner
+
+# First party modules
+from pynball import pynball
+
+
+def test_add(mock_get_env):
+    runner = CliRunner()
+    new_path = str(pytest.PYTHON_DIR / "3.8.10")
+    result = runner.invoke(pynball.versions)
+    assert result.exit_code == 0
+    assert result.output == (
+        "WARNING: System Interpreter is not configured\n"
+        "3.6.8     D:\\PYTHON "
+        "PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.6.8\n"
+        "3.7.9     D:\\PYTHON "
+        "PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.7.9\n"
+        "3.9.10    D:\\PYTHON "
+        "PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.9.10\n"
+    )
