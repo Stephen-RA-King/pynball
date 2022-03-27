0f081f6e77114dc23e5884bb46efc33b83c584b9
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-03-27 14:57:20 +0100

    chore: Use output from new ficture

--- tests/test__set_pynball.py
+++ tests/test__set_pynball.py
@@ -8,7 +8,7 @@ import pytest
 from pynball import pynball
 
 
-def test_set_pynball1(capsys, mock_set_env):
+def test_set_pynball1(capsys, mock_set_env_output):
     dict_object = {"3.6": Path("D:\\Python\\python3.6")}
     pynball._set_pynball(dict_object)
     output, error = capsys.readouterr()
