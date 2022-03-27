a7d5357045856da79ae0497ea9ba2775e8a12480
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-03-27 14:55:37 +0100

    chore: Remove dead code

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -9,9 +9,8 @@ from click.testing import CliRunner
 from pynball import pynball
 
 
-def test_add(mock_get_env):
+def test_versions(mock_get_env):
     runner = CliRunner()
-    new_path = str(pytest.PYTHON_DIR / "3.8.10")
     result = runner.invoke(pynball.versions)
     assert result.exit_code == 0
     assert result.output == (
