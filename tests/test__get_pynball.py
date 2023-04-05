abf302d7b81ce7a45122e8681f7e99977a327ab2
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2023-04-05 15:23:58 +0100

    test: replace hard coded directories with relative ones

--- tests/test__get_pynball.py
+++ tests/test__get_pynball.py
@@ -7,8 +7,11 @@ import pytest
 # First party modules
 from pynball import pynball
 
+BASE = WindowsPath(__file__).resolve().parents[3]
+VERSIONS = ["3.6.8", "3.7.9", "3.9.10"]
 
-def test_get_pynball_notype(capsys, mock_get_env):
+
+def test_get_pynball_notype(capsys, mock_get_env) -> None:
     pynball._get_pynball("nosuchthing", "PYNBALL")
     captured = capsys.readouterr()
     assert (
@@ -17,8 +20,9 @@ def test_get_pynball_notype(capsys, mock_get_env):
     )
 
 
-def test_get_pynball_string(mock_get_env):
-    assert pynball._get_pynball("string", "PYNBALL") == (
+def test_get_pynball_string(mock_get_env) -> None:
+    """generated string should look similar to the following
+    pynball._get_pynball("string", "PYNBALL") == (
         "{'3.6.8': 'D:\\\\PYTHON "
         "PROJECT\\\\PROJECTS\\\\DEV\\\\pynball\\\\pynball\\\\env\\\\PYTHON\\\\3.6.8', "
         "'3.7.9': 'D:\\\\PYTHON "
@@ -26,10 +30,15 @@ def test_get_pynball_string(mock_get_env):
         "'3.9.10': 'D:\\\\PYTHON "
         "PROJECT\\\\PROJECTS\\\\DEV\\\\pynball\\\\pynball\\\\env\\\\PYTHON\\\\3.9.10'}"
     )
+    """
+    get_str = pynball._get_pynball("string", "PYNBALL")
+    for ver in VERSIONS:
+        assert f"pynball\\\\pynball\\\\env\\\\PYTHON\\\\{ver}" in get_str
 
 
-def test_get_pynball_dict(mock_get_env):
-    assert pynball._get_pynball("dict", "PYNBALL") == {
+def test_get_pynball_dict(mock_get_env) -> None:
+    """Generated dictionary should look similar to the following
+    pynball._get_pynball("dict", "PYNBALL") == {
         "3.6.8": "D:\\PYTHON "
         "PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.6.8",
         "3.7.9": "D:\\PYTHON "
@@ -37,10 +46,15 @@ def test_get_pynball_dict(mock_get_env):
         "3.9.10": "D:\\PYTHON "
         "PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.9.10",
     }
+    """
+    get_dict = pynball._get_pynball("dict", "PYNBALL")
+    for ver in VERSIONS:
+        assert f"pynball\\pynball\\env\\PYTHON\\{ver}" in get_dict[f"{ver}"]
 
 
-def test_get_pynball_object(mock_get_env):
-    assert pynball._get_pynball("dict_path_object", "PYNBALL") == {
+def test_get_pynball_object(mock_get_env) -> None:
+    """Generated dictionary should look similar to the following:
+    pynball._get_pynball("dict_path_object", "PYNBALL") == {
         "3.6.8": WindowsPath(
             "D:/PYTHON PROJECT/PROJECTS/DEV/pynball/pynball/env/PYTHON/3.6.8"
         ),
@@ -51,15 +65,26 @@ def test_get_pynball_object(mock_get_env):
             "D:/PYTHON PROJECT/PROJECTS/DEV/pynball/pynball/env/PYTHON/3.9.10"
         ),
     }
+    """
+    path_object_dict = pynball._get_pynball("dict_path_object", "PYNBALL")
+    for ver in VERSIONS:
+        assert str(WindowsPath(f"pynball/pynball/env/PYTHON/{ver}")) in str(
+            path_object_dict[f"{ver}"]
+        )
 
 
-def test_get_pynball_names(mock_get_env):
+def test_get_pynball_names(mock_get_env) -> None:
     assert pynball._get_pynball("names", "PYNBALL") == ["3.6.8", "3.7.9", "3.9.10"]
 
 
-def test_get_pynball_paths(mock_get_env):
+def test_get_pynball_paths(mock_get_env) -> None:
+    """Generated list should look similar to the following
     assert pynball._get_pynball("paths", "PYNBALL") == [
         "D:\\PYTHON PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.6.8",
         "D:\\PYTHON PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.7.9",
         "D:\\PYTHON PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.9.10",
     ]
+    """
+    paths_list = pynball._get_pynball("paths", "PYNBALL")
+    for index, ver in enumerate(VERSIONS):
+        assert f"pynball\\pynball\\env\\PYTHON\\{ver}" in paths_list[index]
--- tests/test_versions.py
+++ tests/test_versions.py
@@ -8,11 +8,14 @@ from click.testing import CliRunner
 # First party modules
 from pynball import pynball
 
+VERSIONS = ["3.6.8", "3.7.9", "3.9.10"]
 
-def test_versions(mock_get_env):
+
+def test_versions(mock_get_env) -> None:
     runner = CliRunner()
     result = runner.invoke(pynball.versions)
     assert result.exit_code == 0
+    """
     assert result.output == (
         "WARNING: System Interpreter is not configured\n"
         "3.6.8     D:\\PYTHON "
@@ -22,3 +25,8 @@ def test_versions(mock_get_env):
         "3.9.10    D:\\PYTHON "
         "PROJECT\\PROJECTS\\DEV\\pynball\\pynball\\env\\PYTHON\\3.9.10\n"
     )
+    """
+
+    assert "WARNING: System Interpreter is not configured" in result.output
+    for ver in VERSIONS:
+        assert f"pynball\\pynball\\env\\PYTHON\\{ver}" in result.output
