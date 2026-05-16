6f9317a4d6e841157ff223933461a657d1818a12
Stephen King <33905365+Stephen-RA-King@users.noreply.github.com>
2026-05-16 18:27:36 +0100

    fix: psr version templating

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # -- Project information -----------------------------------------------------
 
 project = "pynball"
-version = "1.5.7"
+version = "1.5.8"
 copyright = "2022, Stephen R A King"
 author = "Stephen R A King"
 
