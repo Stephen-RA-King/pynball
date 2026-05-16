f826122ca1eb85e302d99678a31692b1720c9ef1
Stephen King <33905365+Stephen-RA-King@users.noreply.github.com>
2026-05-16 16:13:46 +0100

    chore: bump versions

--- src/pynball/__init__.py
+++ src/pynball/__init__.py
@@ -2,7 +2,7 @@
 """Top-level package for _pynball."""
 
 __title__ = "_pynball"
-__version__ = "1.5.7"
+__version__ = "1.5.8"
 __author__ = "Stephen R A King"
 __description__ = "Utility command line to switch between python versions"
 __license__ = "MIT"
